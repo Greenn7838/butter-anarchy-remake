e983ff38340b67968783424bdf5f67551307ad7f
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-01-08 21:59:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,35 @@ client.categories = fs.readdirSync('./commands');
 
 ['command', 'event'].forEach(handler => require(`./handlers/${handler}`)(client));
 
-const createBot = require('./minebot');
-createBot(client);
+const startMC = (client) => {
+    let retry = 0;
+    if (retry <= 0) {
+        client.channels.cache.get(process.env.DISCORD_LIVECHAT).send(
+            new Discord.MessageEmbed()
+                .setColor("YELLOW")
+                .setDescription('Đang kết nối lại tới server')
+        );
+        client.mcTimeout = setTimeout(() => { try { retry++; execute() } catch(e) { console.log(e) } }, 60 * 1000);
+    }
+
+const execute = () => {
+    if (!client.isReady()) return client.channels.cache.get(process.env.DISCORD_LIVECHAT).send(
+        new Discord.MessageEmbed()
+            .setColor('RED')
+            .setDescription('Client chưa sẵn sàng')
+    );
+
+    client.channels.cache.get(process.env.DISCORD_LIVECHAT).send(
+        new Discord.MessageEmbed()
+            .setColor('YELLOW')
+            .setDescription('Đang kết nối lại...')
+    );
+
+    try { require('./minebot')(client); } catch(e) { console.log(e) } 
+    };
+
+    clearTimeout(client.mcTimeout == 0 ? undefined : client.mcTimeout);
+};
 //require('./web')(app);
 
 client.login(process.env.DISCORD_TOKEN);
