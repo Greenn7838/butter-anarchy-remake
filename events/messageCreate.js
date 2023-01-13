b597cea978e326c69a0babb42dca4f2cb2165894
Greenn7838 <103121715+Greenn7838@users.noreply.github.com>
2023-01-13 12:27:58 +0700

    upload

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 require('dotenv').config();
-const Levels = require('discord-xp');
-const ms = require('ms');
 /**
  * 
  * @param {Discord.Client} client 
@@ -11,14 +9,7 @@ module.exports = async(client, message) => {
     if (!message.guild) return;
     if (message.author.bot) return;
 
-    const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
-    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
-    if (hasLeveledUp) {
-      const user = await Levels.fetch(message.author.id, message.guild.id);
-      message.channel.send({ content: `${message.author}, chúc mừng bạn đã lên level **${user.level}**! :tada:` });
-    }
-
-    if (message.content.startsWith(process.env.PREFIX)) {
+    if (!message.content.startsWith(process.env.PREFIX)) return;
         const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
         const cmd = args.shift().toLowerCase();
         let command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
@@ -38,8 +29,5 @@ module.exports = async(client, message) => {
                 setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
             }
             command.run(client,message,args);
-        }    
-    } else {
-        return;
-    }
+        }
 }
--- index.js
+++ index.js
@@ -34,6 +34,17 @@ mongoose.connect(process.env.MONGODB, {
 
 mongoose.set('strictQuery', false);
 
+client.on('messageCreate', async(message) => {
+    if (!message.guild) return;
+    if (message.author.bot) return;
+    const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
+    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+    if (hasLeveledUp) {
+      const user = await Levels.fetch(message.author.id, message.guild.id);
+      message.channel.send({ content: `${message.author}, chúc mừng bạn đã lên level **${user.level}**! :tada:` });
+    }
+})
+
 client.balance = async(id) => {
     let profile = await balance.findOne({ userId: id });
     if (profile) {
